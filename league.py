fef8f10764861fcb1bd33743e0c67e762cf24650
Keagan Jarvis <keaganjarvis@gmail.com>
2018-10-18 10:55:26 +0200

    DB insertions for first teams' first appearance

--- league.py
+++ league.py
@@ -5,26 +5,67 @@ from tinydb import TinyDB, Query
 #EITHER using stdin/stdout OR taking filenames on the command line is fine
 #You can expect that the input will be well-formed. There is no need to add special handling for malformed input files.
 #NB supporting tests needed
-def teams_and_scores(match):
-    import pudb
-    # pudb.set_trace()
-    team_a, team_b = match.split(',')
-    team_b = team_b[1:] #get rid of space at beggining of team_b
-    score_a = team_a[-1]
-    team_a = team_a[:-2] #drop score from team name
-    score_b = team_b[-1]    
-    team_b = team_b[:-2] #drop score from team name
-    print (team_a)
-    print (score_a)
-    print (team_b)
-    print (score_b)
+#TODO doc strings
 
+class Match_Result:
+
+    team_a = None
+    team_b = None
+    score_a = None
+    score_b = None
+    points_a = None
+    points_b = None
+
+    def __init__(self,match):
+        team_a, team_b = match.split(',')
+        team_b = team_b[1:] #get rid of space at beggining of team_b
+        self.score_a = int(team_a[-1])
+        self.team_a = team_a[:-2] #drop score from team name
+        self.score_b = int(team_b[-1])
+        self.team_b = team_b[:-2] #drop score from team name
+        # print (team_a)
+        # print (score_a)
+        # print (team_b)
+        # print (score_b)
+    
+    def getResult(self):
+        if self.score_a > self.score_b:
+            self.points_a = 3
+            self.points_b = 0
+        elif self.score_b > self.score_a:
+            self.points_a = 0
+            self.points_b = 3
+        elif self.score_a == self.score_b:
+            self.points_a = 1
+            self.points_b = 1
+     
+def team_exists (db, log, name):
+    
+    query = db.search(log.name == name)
+    if query:
+        return True
+    else:
+        return False
 
 def process_match(match):
     db = TinyDB('soccer_league.json')
     current_standings = Query()
-    teams_and_scores(match)
-    #check result
+    #check result of the match
+    match_result = Match_Result(match)
+    match_result.getResult()
+    # import pudb; pudb.set_trace()
+    if team_exists(db,current_standings,match_result.team_a):
+        #add the points onto the existing points
+        print match_result.team_a
+    else:
+        db.insert({'name': match_result.team_a, 'points': match_result.points_a})
+    #TODO repeating oneself...
+    if team_exists(db,current_standings,match_result.team_b):
+        #add the points onto the existing points
+        print match_result.team_b
+    else:
+        db.insert({'name': match_result.team_b, 'points': match_result.points_b})
+    
     #update the table with points
     #if team exists in DB, increase their points
     #else, insert team into DB with their points
